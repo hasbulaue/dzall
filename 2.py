d16c0e11afecaf7e2ba382b177791748996a3a76
Vladimir Skvorcov <133113815+hasbulaue@users.noreply.github.com>
2025-07-31 19:50:14 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,45 @@
+from turtle import *
+screensize(50000,50000)
+pensize(5)
+color("red")
+speed (10)
+for i in range(4):
+    fd(500)
+    lt(90)
+
+penup()
+lt(90)
+fd(500)
+pendown()
+
+rt(45)
+fd(354)
+rt(90)
+fd(354)
+penup()
+home()
+fd(50)
+pendown()
+lt(90)
+fd(400)
+rt(90)
+fd(200)
+rt(90)
+fd(400)
+lt(90)
+
+
+penup()
+fd(200)
+lt(90)
+fd(350)
+pendown()
+
+for i in range(4):
+    lt(90)
+    fd(150)
+    
+
+
+
+done()
